dbc458900ff9f0b6b59c6e4ca91ddd94c31a4bfb
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 00:51:18 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("1", "M
 ("3", "Tabitha", 2, "F", "Black", "Calm", 1);
 ("4", "Sergeant Brown", 1, "M", "Brown", "Aggressive", 1);
 ("5", "Melissa", 4, "F", "Red", "Calm", 1);
-("6", Grinch", 3, "F", "Green", "Grumpy", 1);
-("7", Wendy", 5, "F", "Blonde", "Calm", 1);
+("6", "Grinch", 3, "F", "Green", "Grumpy", 1);
+("7", "Wendy", 5, "F", "Blonde", "Calm", 1);
 ("8", NULL, 1, "M", "Brown", "Nervous", 1);
