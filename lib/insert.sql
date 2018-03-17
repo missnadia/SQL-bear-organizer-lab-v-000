5d31b2c512169f0493813f2b4344922bf0ac687c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 00:35:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Active", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "Black", "Calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Brown", "Aggressive", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Red", "Calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "F", "Green", "Grumpy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "F", "Blonde", "Calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "M", "Brown", "Nervous", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "Black", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Red", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "F", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "F", "Blonde", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "M", "Brown", "Nervous", 1);
