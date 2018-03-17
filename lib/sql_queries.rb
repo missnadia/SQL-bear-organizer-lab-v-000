54d5b2bf34340d58cef6293ff9f352d04477f87a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 00:48:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT "
+  "SELECT * FROM bears MAX(color)"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  ""
+  "SELECT * bears WHERE temperament = goofy"
 end
 
 def selects_bear_that_killed_Tim
-  ""
+  "SELECT * bears WHERE temperament = aggressive"
 end
