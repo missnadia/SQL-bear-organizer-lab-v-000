b8a5d1d4ea6b5a821691890ffbda7980f5fc8b8b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 00:57:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("1", "Mr. Chocolate", 1, "M", "Brown", "Goofy", 1);
-("2", "Rowdy", 2, "M", "Brown", "Active", 1);
-("3", "Tabitha", 2, "F", "Black", "Calm", 1);
-("4", "Sergeant Brown", 1, "M", "Brown", "Aggressive", 1);
-("5", "Melissa", 4, "F", "Red", "Calm", 1);
-("6", "Grinch", 3, "F", "Green", "Grumpy", 1);
-("7", "Wendy", 5, "F", "Blonde", "Calm", 1);
-("8", NULL, 1, "M", "Brown", "Nervous", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
